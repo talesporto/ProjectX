3052942fb9c68e2c165727c2031910afab18b4a0
Tales Porto <talesap@gmail.com>
2012-04-10 21:56:47 -0300

    Innitial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectX::Application.config.secret_token = '5869eadf6b94e0334db60d42a4ab3e27df8cc8435693fd0520f5a27630fdfee4f73a93ecbab53f8952d3aa8100a491f55c9aad3d47f2b49ad8032eb93cd25c78'
